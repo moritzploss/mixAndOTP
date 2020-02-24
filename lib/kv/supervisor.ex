6dbf984ef44047d0e0cefbb6c8288369a5f939ed
Moritz <moritz.ploss@gmail.com>
2020-02-24 21:07:19 +0100

    update supervision strategy

--- lib/kv/supervisor.ex
+++ lib/kv/supervisor.ex
@@ -8,10 +8,10 @@ defmodule KV.Supervisor do
   @impl true
   def init(:ok) do
     children = [
-      {KV.Registry, name: KV.Registry},
-      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one},
+      {KV.Registry, name: KV.Registry}
     ]
     # invokes `child_spec/1` on each child module
-    Supervisor.init(children, strategy: :one_for_one)
+    Supervisor.init(children, strategy: :one_for_all)
   end
 end
